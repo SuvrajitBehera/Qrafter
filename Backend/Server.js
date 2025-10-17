5aa53a037db32a9a0b9b738ba793789b414dfb0f
Suvrajit Behera <suvrajitbehera898@gmail.com>
2025-10-18 00:50:11 +0530

    Revert

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,8 +2,11 @@ import bodyParser from 'body-parser';
 import express from 'express'
 import {createWriteStream , readFile,unlink,readdir} from 'fs'
 import {image} from 'qr-image'
-import {resolve , join} from 'path' 
+import {resolve , join,dirname} from 'path' 
+import {fileURLToPath} from 'url'
 let imgCount = 0
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(resolve(__filename,'..'));
 function createQR(URL){
     return new Promise((res,rej)=>{
         if(res){
@@ -21,7 +24,7 @@ function createQR(URL){
 function generateSvg(data){
     const imagePath = '/data/'
     app.get(imagePath+data , (req,res)=>{
-        res.sendFile(join(resolve(__filename,'..'),'data',data))
+        res.sendFile(join(dirname(__filename,'..'),'data',data))
     })
     return (imagePath+data)
     
@@ -30,10 +33,10 @@ const app = express()
 const port = 3000;
 const api = '/api'
 app.use(bodyParser.urlencoded({extended:true}))
-app.use(express.static(join(resolve('..'),'Frontend','public')))
+app.use(express.static(join(__dirname,'Frontend','public')))
 
 app.get('/' ,(req,res)=>{
-    res.sendFile(join(resolve('..'),'Frontend','index.html'))
+    res.sendFile(join(__dirname,'Frontend','index.html'))
 })
 app.get(api,(req,res)=>{
     const url = req.query.url;
@@ -43,13 +46,11 @@ app.get(api,(req,res)=>{
     .then((data)=>{
         let imgData = {url: data}
         res.setHeader("Content-Type" , "application/json")
-        res.setHeader('Access-Control-Allow-Origin','https://qrafter.vercel.app')
         res.send(JSON.stringify(imgData))
     })
 })
 app.get(api+'/download' , (req,res)=>{
     let file = req.query.path
-    res.setHeader('Access-Control-Allow-Origin','https://qrafter.vercel.app')
     res.download(join(resolve(),file) , 'qr.svg')
 })
 app.listen(port,console.log(`server is live at ${port}`))
